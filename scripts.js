b8afd67b94193685894dc7e63d7dc5b729a0fe33
Victor Matias <vmatiasdev@gmail.com>
2025-08-27 16:51:05 -0300

    Versão 1.0

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
