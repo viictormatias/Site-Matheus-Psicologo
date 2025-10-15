a96616c13544df7722fcf626bea82bcfaffabf37
Victor Matias <vmatiasdev@gmail.com>
2025-10-15 19:32:04 -0300

    Bloqueios elfsight

--- scripts.js
+++ scripts.js
@@ -13,3 +13,13 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach((element) => myObserver.observe(element))
+
+
+// Execute esta função APÓS alguns segundos (Ajuste o tempo se necessário)
+setTimeout(function () {
+    const selo = document.querySelector('a[href*="elfsight.com"][target="_blank"]');
+    if (selo) {
+        selo.remove();
+        console.log("Selo da Elfsight removido após estabilização.");
+    }
+}, 3000); // 3 segundos
